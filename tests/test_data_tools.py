2e31cce921c1e70bf2f88cef83b5e6f7f85071b7
Ivan Valles Perez <ivanvallesperez@gmail.com>
2019-01-26 20:00:53 +0100

    Add: effects and proper tests

--- tests/test_data_tools.py
+++ tests/test_data_tools.py
@@ -1,11 +1,9 @@
 from unittest import TestCase
-import torch
-from torch import nn
 
 import numpy as np
 
-from src.data_tools import read_wavfile, normalize_wavfile, fix_wavfile_duration,\
-    fix_wavfile_length, saturate_wavfile, resample_wavfile, time_offset_wavfile, add_noise_to_wavfile,\
+from src.data_tools import read_wavfile, normalize_wavfile, fix_wavfile_duration, \
+    fix_wavfile_length, saturate_wavfile, resample_wavfile, time_offset_wavfile, add_noise_to_wavfile, \
     pitch_shift_wavfile, randomly_distort_wavfile
 
 
@@ -29,7 +27,7 @@ class TestDataTools(TestCase):
         wav_0 = wav / 32768
         wav_1 = wav_0 * 2147483648
         wav_2 = wav
-        wav_3 = 255*(wav_0+1)/2
+        wav_3 = 255 * (wav_0 + 1) / 2
         wav_0_processed = normalize_wavfile(wav_0, 0)
         wav_1_processed = normalize_wavfile(wav_1, 1)
         wav_2_processed = normalize_wavfile(wav_2, 2)
@@ -51,15 +49,15 @@ class TestDataTools(TestCase):
     def test_fix_wavfile_duration(self):
         sample_rate, wav = read_wavfile(self.wav_filepath)
 
-        wav_processed = fix_wavfile_duration(wav=wav, sample_rate=sample_rate, duration=len(wav)/sample_rate)
+        wav_processed = fix_wavfile_duration(wav=wav, sample_rate=sample_rate, duration=len(wav) / sample_rate)
         self.assertListEqual(list(wav), list(wav_processed))
 
         wav_processed = fix_wavfile_duration(wav=wav, sample_rate=sample_rate, duration=0.3)
-        self.assertEqual(len(wav_processed), int(0.3*sample_rate))
+        self.assertEqual(len(wav_processed), int(0.3 * sample_rate))
 
         wav_processed = fix_wavfile_duration(wav=wav, sample_rate=sample_rate, duration=3)
         self.assertEqual(len(wav_processed), int(3 * sample_rate))
-        self.assertListEqual([0]*16000, list(wav_processed[:16000]))
+        self.assertListEqual([0] * 16000, list(wav_processed[:16000]))
         self.assertListEqual([0] * 16000, list(wav_processed[-16000:]))
         self.assertEqual(np.sum(wav), np.sum(wav_processed))
 
@@ -84,11 +82,11 @@ class TestDataTools(TestCase):
         wav = normalize_wavfile(wav, normalize_to_peak=True)
         wav_processed = saturate_wavfile(wav, 1)
         self.assertListEqual(wav.tolist(), wav_processed.tolist())
-        wav_processed = saturate_wavfile(wav, 2) # Atenuate
+        wav_processed = saturate_wavfile(wav, 2)  # Atenuate
         self.assertAlmostEqual(wav.mean(), wav_processed.mean(), delta=0.05)
         self.assertGreater(np.std(wav), np.std(wav_processed))
         self.assertGreater(max(abs(wav)), max(abs(wav_processed)))
-        wav_processed = saturate_wavfile(wav, 0.2) # Saturate
+        wav_processed = saturate_wavfile(wav, 0.2)  # Saturate
         self.assertAlmostEqual(wav.mean(), wav_processed.mean(), delta=0.05)
         self.assertLess(np.std(wav), np.std(wav_processed))
         self.assertEqual(max(abs(wav)), max(abs(wav_processed)))
@@ -110,16 +108,16 @@ class TestDataTools(TestCase):
         wav_processed = time_offset_wavfile(wav=wav, shift_factor=0)
         self.assertEqual(len(wav), len(wav_processed))
         self.assertAlmostEqual(wav.sum(), wav_processed.sum(), delta=0.00001)
-        wav_processed = time_offset_wavfile(wav=wav, shift_factor=1) # Full_shift
+        wav_processed = time_offset_wavfile(wav=wav, shift_factor=1)  # Full_shift
         self.assertEqual(np.abs(wav_processed).sum(), 0)
-        wav_processed = time_offset_wavfile(wav=wav, shift_factor=-1) # Full shift inverse
+        wav_processed = time_offset_wavfile(wav=wav, shift_factor=-1)  # Full shift inverse
         self.assertEqual(np.abs(wav_processed).sum(), 0)
-        wav_processed = time_offset_wavfile(wav=wav, shift_factor=0.5) # Half shift
+        wav_processed = time_offset_wavfile(wav=wav, shift_factor=0.5)  # Half shift
         self.assertEqual(len(wav), len(wav_processed))
-        self.assertListEqual(wav.tolist()[:int(len(wav)/2)], wav_processed.tolist()[-int(len(wav)/2):])
-        wav_processed = time_offset_wavfile(wav=wav, shift_factor=-0.5) # Half shift inverse
+        self.assertListEqual(wav.tolist()[:int(len(wav) / 2)], wav_processed.tolist()[-int(len(wav) / 2):])
+        wav_processed = time_offset_wavfile(wav=wav, shift_factor=-0.5)  # Half shift inverse
         self.assertEqual(len(wav), len(wav_processed))
-        self.assertListEqual(wav.tolist()[-int(len(wav)/2):], wav_processed.tolist()[:int(len(wav)/2)])
+        self.assertListEqual(wav.tolist()[-int(len(wav) / 2):], wav_processed.tolist()[:int(len(wav) / 2)])
 
     def test_add_noise(self):
         sample_rate, wav = read_wavfile(self.wav_filepath)
@@ -154,7 +152,7 @@ class TestDataTools(TestCase):
         fft_wav_proc = fft_wav_proc[:int(len(fft_wav_proc) / 2)]
         fft_wav_proc = np.abs(fft_wav_proc)
         self.assertLess(np.argmax(fft_wav), np.argmax(fft_wav_proc))
-        self.assertEqual(np.max(np.abs(wav)), np.max(np.abs(wav_processed))) # Check peak
+        self.assertEqual(np.max(np.abs(wav)), np.max(np.abs(wav_processed)))  # Check peak
 
         wav_processed = pitch_shift_wavfile(wav=wav, sr=sample_rate, n_octaves=-0.5)
         self.assertEqual(len(wav), len(wav_processed))
@@ -174,5 +172,5 @@ class TestDataTools(TestCase):
         self.assertEqual(len(wav), len(wav_processed))
         peak = max(np.abs(np.max(wav)), np.abs(np.min(wav)))
         new_peak = max(np.abs(np.max(wav)), np.abs(np.min(wav)))
-        self.assertAlmostEqual(peak, new_peak, delta=0.5*peak)
-        self.assertAlmostEqual(np.mean(wav), np.mean(wav_processed), delta=0.1*peak)
+        self.assertAlmostEqual(peak, new_peak, delta=0.5 * peak)
+        self.assertAlmostEqual(np.mean(wav), np.mean(wav_processed), delta=0.1 * peak)
